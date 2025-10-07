4b1b5f8975727c068939077dfd24080abef8710f
Sam <633856+SamP20@users.noreply.github.com>
2025-10-07 23:54:10 +0100

    Initial Session Manager

---
+++ portal/systems/session_manager.py
@@ -0,0 +1,184 @@
+
+
+from datetime import datetime, timedelta, timezone
+import functools
+from flask import Flask, Response, current_app, g, request
+from flask_sqlalchemy import SQLAlchemy
+import hashlib
+import secrets
+import uuid
+
+from portal.models import Session, User
+
+
+class _State:
+    def __init__(self, app: Flask):
+        self.cookie_name: str = app.config.get('HS_SESSION_NAME', "id")
+        self.cookie_max_age = self.as_timedelta(app.config.get('HS_SESSION_MAX_AGE', timedelta(days=30)))
+        self.cookie_secure: bool = app.config.get('HS_SESSION_SECURE', False)
+
+        self.keyfob_max_idle = self.as_timedelta(app.config.get("HS_KEYFOB_MAX_IDLE", timedelta(minutes=20)))
+        self.login_max_idle = self.as_timedelta(app.config.get("HS_LOGIN_MAX_IDLE", timedelta(days=30)))
+        self.elevated_auth_expiry = self.as_timedelta(app.config.get("HS_ELEVATED_AUTH_EXPIRY", timedelta(minutes=20)))
+
+
+
+    @staticmethod
+    def as_timedelta(value: int|float|timedelta) -> timedelta:
+        if not isinstance(value, timedelta):
+            value = timedelta(seconds=value)
+        return value
+
+
+class SessionManager:
+    def __init__(self, db: SQLAlchemy, app:Flask|None):
+        self.db = db
+
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app: Flask):
+        app.before_request(self.load_session)
+
+    @property
+    def _state(self) -> _State:
+        state = current_app.extensions["hs.portal.session"]
+        return state
+
+    def load_session(self):
+        parts = request.cookies.get(self._state.cookie_name, "").split(":")
+        if len(parts) != 2:
+            return
+        id_, secret = parts
+
+        session = self.db.session.get(Session, id_)
+
+        if session is None:
+            return
+
+        if not secrets.compare_digest(session.secret_hash, self.hash_secret(secret)):
+            return
+
+        contexts = self.calculate_auth_contexts(session)
+
+        if contexts:
+            session.last_active = datetime.now(timezone.utc)
+            g.hs_session = session
+            g.hs_session_ctx = contexts
+
+            current_app.after_request(functools.partial(self.update_cookie, session, secret))
+        else:
+            self.db.session.delete(session)
+        self.db.session.commit()
+
+    def authenticate_session(self, user: User, methods: dict[str, datetime]):
+        """Create or authenticate a session using the provided methods
+
+        If a session doesn't currently exist then a new one will be created with the provided user.
+
+        The session will be updated with the authentication times provided in the methods.
+
+        If the current session was for a different user then it will be logged out before creating a new one.
+
+        :param user: User to authenticate
+        :param methods: Dictionary containing authentication method names and timestamps
+        """
+        now = datetime.now(timezone.utc)
+
+        session: Session|None = g.get('hs_session')
+        # If the current session is for a different user then delete it
+        if session and session.user != user:
+            self.db.session.delete(session)
+            self.db.session.commit()
+            session = None
+
+        if session is None:
+            session = Session(
+                id=uuid.uuid4(),
+                created=now,
+                user=user,
+                last_active=now
+            )
+            self.db.session.add(session)
+            g.hs_session = session
+
+        # Rotate secret
+        secret = secrets.token_urlsafe()
+        session.secret_hash = self.hash_secret(secret)
+
+        for method, auth_time in methods.items():
+            match method:
+                case 'email':
+                    session.last_email_auth = auth_time
+                case 'keyfob':
+                    session.last_keyfob_auth = auth_time
+                case 'totp':
+                    session.last_totp_auth = auth_time
+                case 'passkey':
+                    session.last_passkey_auth = auth_time
+                case _:
+                    raise ValueError(f"Invalid method {method}")
+
+        self.db.session.commit()
+
+        #TODO: remove update_cookie call from load_session as this will overwrite it
+        current_app.after_request(functools.partial(self.update_cookie, session, secret))
+
+    def update_cookie(self, session: Session, secret: str, response: Response) -> Response:
+        value = f"{session.id.hex}:{secret}"
+        response.set_cookie(
+            key=self._state.cookie_name,
+            value=value,
+            max_age=self._state.cookie_max_age,
+            httponly=True,
+            secure=self._state.cookie_secure
+        )
+        return response
+
+    def calculate_auth_contexts(self, session: Session) -> set[str]:
+        """Calculate authentication contexts based on the current session state
+
+        In general the contexts are as follows:
+        * ``plastic`` for keyfob logins
+        * ``bronze`` for regular logins of any age (within the ``HS_LOGIN_MAX_IDLE`` limit)
+        * ``silver`` for recently authenticated single factor (email) logins
+        * ``gold`` for recently authenticated two factor (TOTP/passkey) logins
+
+        :param session: Session to get contexts for
+        :returns: Set of contexts for this session
+        """
+        contexts = set()
+
+        now = datetime.now(timezone.utc)
+
+        if session.last_keyfob_auth:
+            if now < session.last_active + self._state.keyfob_max_idle:
+                contexts.add("plastic")
+
+        if (session.last_email_auth or session.last_passkey_auth or session.last_totp_auth):
+            if now < session.last_active + self._state.login_max_idle:
+                contexts.add("bronze")
+
+        if session.last_email_auth and now < session.last_email_auth + self._state.elevated_auth_expiry:
+            contexts.add("silver")
+
+        if session.last_totp_auth and now < session.last_totp_auth + self._state.elevated_auth_expiry:
+            contexts.add("silver")
+            contexts.add("gold")
+
+        if session.last_passkey_auth and now < session.last_passkey_auth + self._state.elevated_auth_expiry:
+            contexts.add("silver")
+            contexts.add("gold")
+
+        return contexts
+
+    def has_context(self, ctx: str) -> bool:
+        if 'hs_session_ctx' not in g:
+            return False
+        return ctx in g.hs_session_ctx
+
+    @staticmethod
+    def hash_secret(secret: str|bytes) -> str:
+        if isinstance(secret, str):
+            secret = secret.encode("utf-8")
+        return hashlib.sha256(secret).hexdigest()
