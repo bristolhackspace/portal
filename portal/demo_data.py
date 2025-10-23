369e4c10bf32c351706c3ab095a765bb3e542453
Sam <633856+SamP20@users.noreply.github.com>
2025-10-23 14:29:17 +0100

    Add OAuth client and demo user to demo data

--- portal/demo_data.py
+++ portal/demo_data.py
@@ -1,6 +1,8 @@
+import uuid
 from flask import Blueprint
 
-from portal.extensions import db
+from portal.extensions import db, jwks
+from portal.models import OAuthClient, User
 
 bp = Blueprint('demo', __name__, cli_group=None)
 
@@ -10,3 +12,18 @@ def make_demo_data():
     db.drop_all()
     db.create_all()
 
+    demo_client = OAuthClient(
+        id=uuid.UUID(hex="aaf36b4c-5e88-409f-863d-24f12f0ec111"),
+        name="Demo client"
+    )
+    db.session.add(demo_client)
+
+    demo_user = User(
+        display_name="Demo User",
+        email="example@example.com"
+    )
+    db.session.add(demo_user)
+
+    jwks.rotate_keys()
+
+    db.session.commit()
