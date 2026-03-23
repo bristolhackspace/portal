2fa5db7240fda1905744e52410d81c3e257b91ee
Sam <633856+SamP20@users.noreply.github.com>
2026-03-23 23:26:18 +0000

    Fix login_required decorator

--- portal/decorators.py
+++ portal/decorators.py
@@ -1,13 +1,13 @@
 from datetime import datetime, timezone
-from flask import request, url_for
+from flask import redirect, request, url_for
 import functools
 
-from portal.extensions import session_manager, authentication
+from portal.extensions import hs
 
 def login_required(f):
     @functools.wraps(f)
     def wrapper_fn(*args, **kwargs):
-        current_session = session_manager.current_session
+        current_session = hs.session.current_session
 
         try:
             max_age = int(request.values.get("max_age", ""))
@@ -17,7 +17,7 @@ def login_required(f):
         now = datetime.now(timezone.utc)
 
         if current_session is None or (max_age is not None and (now - current_session.last_auth).total_seconds() > max_age):
-            authentication.begin_flow(request.url)
+            hs.authentication.begin_flow(request.url)
             return redirect(url_for("login.index"))
         return f(*args, **kwargs)
     return wrapper_fn
