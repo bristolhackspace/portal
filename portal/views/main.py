98b5560588d3d672507015eaee0b37d363336eea
Sam <633856+SamP20@users.noreply.github.com>
2026-03-27 14:42:41 +0000

    Replace decorators with before_request hooks

--- portal/views/main.py
+++ portal/views/main.py
@@ -1,21 +1,17 @@
 from flask import Blueprint, redirect, render_template, url_for
 import sqlalchemy as sa
 
-from portal.decorators import login_required
+from portal.middleware import login_required
 from portal.extensions import db, hs
 from portal.models import App
 
 bp = Blueprint("main", __name__, url_prefix="/")
 
+bp.before_request(login_required)
+
 @bp.route("/")
-@login_required
 def index():
     query = sa.select(App).order_by(App.order)
     apps = db.session.execute(query).scalars()
 
     return render_template("main/index.html.j2", apps=apps)
-
-@bp.route("/logout")
-def logout():
-    hs.session_manager.logout()
-    return redirect(url_for(".index"))
