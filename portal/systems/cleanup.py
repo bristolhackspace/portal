fa0808eeb201cb3ce9649b06b233af12aef87e3e
Sam <633856+SamP20@users.noreply.github.com>
2026-04-10 11:50:59 +0100

    Test cleanup system and fix bugs

--- portal/systems/cleanup.py
+++ portal/systems/cleanup.py
@@ -1,31 +1,32 @@
 from collections.abc import Callable
 from flask import Blueprint, Flask, current_app
+from flask.cli import AppGroup
 
 
 class Cleanup:
     def __init__(self, app: Flask):
         self.callbacks: dict[str, Callable[[], int]] = {}
 
-        self.bp = Blueprint('cleanup', __name__)
+        self.cli = AppGroup("cleanup")
 
-        app.register_blueprint(self.bp)
+        app.cli.add_command(self.cli)
 
-        @self.bp.cli.command('all')
+        @self.cli.command('all')
         def cleanup_all():
             for cb in self.callbacks:
                 self.do_cleanup_one(cb)
 
-    def register_callback(self, name, fn: Callable[[], int]):
+    def register_callback(self, name: str, fn: Callable[[], int]):
         """Register a cleanup callback. `fn` must return the
         number of items it has cleaned up."""
         if name == 'all':
             raise ValueError("All is a reserved name")
         self.callbacks[name] = fn
 
-        @self.bp.cli.command(name)
+        @self.cli.command(name)
         def cleanup_one():
             self.do_cleanup_one(name)
 
-    def do_cleanup_one(self, name):
+    def do_cleanup_one(self, name: str):
         num_items = self.callbacks[name]()
         current_app.logger.info(f"Cleaned up {num_items} {name} item(s)")
