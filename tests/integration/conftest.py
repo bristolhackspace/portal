e0cc8199df53c3697dff2f9050bbddeaebd022e0
Sam <633856+SamP20@users.noreply.github.com>
2026-04-10 15:11:13 +0100

    Add login integration tests

---
+++ tests/integration/conftest.py
@@ -0,0 +1,42 @@
+from flask import Flask, current_app
+from flask.testing import FlaskClient
+import pytest
+
+from portal import create_app
+from portal.extensions import db
+from portal.systems.authentication import Authentication
+from portal.systems.cleanup import Cleanup
+from portal.systems.discourse_connect import DiscourseConnect
+from portal.systems.mailer import BaseMailer
+from portal.systems.rate_limiter import RateLimiter
+from portal.systems.session_manager import SessionManager
+
+
+@pytest.fixture()
+def app() -> Flask:
+    app = create_app(
+        test_config=dict(
+            SQLALCHEMY_DATABASE_URI="postgresql+psycopg2://postgres:postgres@localhost:5432/portal_test",
+            TEST_MAILER=True,
+            DISCOURSE_CONNECT_SECRET="test secret",
+            WTF_CSRF_ENABLED=False
+        )
+    )
+    return app
+
+
+@pytest.fixture()
+def app_context(app):
+    with app.app_context():
+        yield current_app
+
+@pytest.fixture()
+def client(app: Flask) -> FlaskClient:
+    return app.test_client()
+
+@pytest.fixture(autouse=True)
+def init_db(app: Flask, app_context: Flask):
+    db.create_all()
+    yield
+    db.session.remove()
+    db.drop_all()
---
+++ tests/integration/test_login.py
@@ -0,0 +1,155 @@
+from typing import cast
+
+import pytest
+from yarl import URL
+from werkzeug.test import TestResponse
+
+from portal.extensions import db
+from portal.extensions import hs
+from portal.models import Member
+from portal.models.authentication import AuthFlow
+from portal.systems.mailer import TestMailer
+
+
+@pytest.fixture()
+def member_model(init_db):
+    member = Member(
+        id=1, display_name="Test Member", email="example@example.com", username="test"
+    )
+    db.session.add(member)
+    db.session.commit()
+    return member
+
+
+@pytest.fixture()
+def member_model_no_username(init_db):
+    member = Member(id=2, display_name="Test Member 2", email="example2@example.com")
+    db.session.add(member)
+    db.session.commit()
+    return member
+
+
+def load_initial_login_page(client):
+    # Load the email entry page
+    response = client.get("/login/", follow_redirects=True)
+    assert response.status_code == 200
+
+
+def check_null_session():
+    # Check we don't currently have a session
+    session = hs.session_manager.current_session
+    assert session == None
+
+
+def request_and_verify_email(
+    client, member_model
+) -> tuple[TestMailer.EmailCapture, TestResponse]:
+    # Submit the email of the test member
+    response = client.post(
+        "/login/", follow_redirects=True, data={"email": member_model.email}
+    )
+    assert response.status_code == 200
+
+    # Check an email got sent with the correct details
+    mailer = cast(TestMailer, hs.mailer)
+    assert len(mailer.captured_emails) == 1
+    email = mailer.captured_emails[0]
+    assert email.member == member_model
+    otp = email.kwargs["otp"]
+    assert len(otp) == 6
+    flow: AuthFlow = email.kwargs["flow"]
+    assert flow.id.hex == response.request.args["flow_id"]
+    return email, response
+
+
+def check_valid_session(member_model: Member):
+    # Check a session was created successfully
+    session = hs.session_manager.current_session
+    assert session != None
+    assert session.member == member_model
+
+
+def submit_otp_form(
+    client, email: TestMailer.EmailCapture, prev_response: TestResponse
+) -> TestResponse:
+    otp = email.kwargs["otp"]
+    # Submit the OTP found in the email. Pass through previous request args for the flow_id
+    response = client.post(
+        "/login/",
+        follow_redirects=True,
+        query_string=prev_response.request.args,
+        data={"otp": otp},
+    )
+    assert response.status_code == 200
+
+    return response
+
+
+def submit_otp_url(client, email: TestMailer.EmailCapture) -> TestResponse:
+    magic_url = URL(email.kwargs["magic_url"])
+
+    # Submit the OTP found in the email. Pass through previous request args for the flow_id
+    response = client.get(
+        magic_url.path, query_string=magic_url.query, follow_redirects=True
+    )
+    assert response.status_code == 200
+
+    return response
+
+
+def submit_username(client, member_model: Member, prev_response: TestResponse) -> TestResponse:
+    username = "test_username"
+    response = client.post(
+        "/login/",
+        follow_redirects=True,
+        query_string=prev_response.request.args,
+        data={"username": username},
+    )
+    assert response.status_code == 200
+
+    assert member_model.username == username
+
+    return response
+
+
+def test_email_login_code(client, member_model):
+    load_initial_login_page(client)
+    check_null_session()
+    email, response = request_and_verify_email(client, member_model)
+
+    response = submit_otp_form(client, email, response)
+
+    # Check we got redirected to the homepage
+    assert response.request.path == "/"
+
+    check_valid_session(member_model)
+
+
+def test_email_login_magic_url(client, member_model):
+    load_initial_login_page(client)
+    check_null_session()
+    email, response = request_and_verify_email(client, member_model)
+
+    response = submit_otp_url(client, email)
+
+    # Check we got redirected to the homepage
+    assert response.request.path == "/"
+
+    check_valid_session(member_model)
+
+def test_email_login_set_username(client, member_model_no_username):
+    load_initial_login_page(client)
+    check_null_session()
+    email, response = request_and_verify_email(client, member_model_no_username)
+
+    response = submit_otp_form(client, email, response)
+
+    # Check we haven't been redirected to the homepage yet
+    assert response.request.path == "/login/"
+
+    response = submit_username(client, member_model_no_username, response)
+
+    # Check we got redirected to the homepage
+    assert response.request.path == "/"
+
+    check_valid_session(member_model_no_username)
