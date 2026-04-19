c6a4bdaec6afee08d0fa35dce6433914b02b908e
Sam <633856+SamP20@users.noreply.github.com>
2026-04-19 19:55:41 +0100

    Add member management pages (#26)

--- portal/views/admin.py
+++ portal/views/admin.py
@@ -1,11 +1,14 @@
 from functools import partial
 
+from flask_wtf import FlaskForm
 import sqlalchemy as sa
 from flask import Blueprint, flash, redirect, render_template, url_for
+from wtforms import EmailField, SelectMultipleField, StringField
+from wtforms.validators import ReadOnly
 
 from portal.extensions import db
 from portal.middleware import login_required
-from portal.models import Member, RateLimit
+from portal.models import Member, RateLimit, Role
 
 bp = Blueprint("admin", __name__, url_prefix="/admin")
 
@@ -21,6 +24,41 @@ def index():
 def members():
     page = db.paginate(db.select(Member).order_by(Member.display_name))
     return render_template("admin/members.html.j2", page=page)
+
+
+class MemberForm(FlaskForm):
+    display_name = StringField("Display name", validators=[ReadOnly()])
+    email = EmailField("Email", validators=[ReadOnly()])
+    username = StringField("Username", validators=[])
+    roles = SelectMultipleField("Roles", coerce=int)
+
+
+@bp.route("/members/<int:member_id>", methods=["GET", "POST"])
+def member(member_id: int):
+    member = db.get_or_404(Member, member_id)
+
+    form = MemberForm(data={
+        "display_name": member.display_name,
+        "email": member.email,
+        "username": member.username,
+        "roles": [role.id for role in member.roles]
+    })
+
+    role_query = db.select(Role).order_by(Role.name)
+    roles = db.session.execute(role_query).scalars().all()
+    form.roles.choices = [(role.id, role.name) for role in roles]
+
+    if form.validate_on_submit():
+        member.username = form.username.data
+        selected_roles = []
+        for role in roles:
+            if role.id in form.roles.data:
+                selected_roles.append(role)
+        member.roles = selected_roles
+        db.session.commit()
+
+        return redirect(url_for(".member", member_id=member.id))
+    return render_template("admin/member.html.j2", member=member, form=form)
 
 
 @bp.route("/rate-limits")
