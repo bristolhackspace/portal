08701f2b9dacdb3bc548d9361bcc8d1e03332f02
Sam <633856+SamP20@users.noreply.github.com>
2026-04-20 13:56:53 +0100

    Add strategic audit logs

--- portal/systems/__init__.py
+++ portal/systems/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+from portal.systems.audit import Audit
 from portal.systems.authentication import Authentication
 from portal.systems.cleanup import Cleanup
 from portal.systems.discourse_connect import DiscourseConnect
@@ -13,10 +14,11 @@ class HackspaceSystems:
     def __init__(self, db: SQLAlchemy, app: Flask):
         self.db = db
         self.cleanup = Cleanup(app)
+        self.audit = Audit(db, app)
         self.rate_limiter = RateLimiter(self.db, app)
-        self.session_manager = SessionManager(self.db, self.cleanup, app)
+        self.session_manager = SessionManager(self.db, self.cleanup, self.audit, app)
         self.mailer = BaseMailer.build(app)
         self.authentication = Authentication(
             self.mailer, self.db, self.rate_limiter, app
         )
-        self.discourse_auth = DiscourseConnect(app)
+        self.discourse_auth = DiscourseConnect(self.audit, app)
